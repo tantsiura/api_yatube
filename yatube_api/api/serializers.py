c93179620cd9f9887f001e45ee1f6842a5241be2
tantsiura <97056496+tantsiura@users.noreply.github.com>
2023-02-21 12:19:54 +0300

    Правки по pep8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,6 @@
-from posts.models import Comment, Group, Post, User
 from rest_framework import serializers
+
+from posts.models import Comment, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
